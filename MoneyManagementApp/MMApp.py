6c305f8b11bf19a08f0842b1c8af46403f63e9e6
sebcampos <sebcampos@criptext.com>
2021-09-18 14:45:07 -0700

    Working on calender

--- MoneyManagementApp/MMApp.py
+++ MoneyManagementApp/MMApp.py
@@ -78,7 +78,9 @@ class DB():
         return dataframe.to_sql(table, if_exists="append", index=False, con=self.conn)
     
     def delete(self, table, value):
-        pass
+        df = self.fetch(table)
+        df.drop(value, inplace=True)
+        return df.to_sql(table, if_exists='replace', index=False, con=self.conn)
 
     def fetch(self, table):
         return pandas.read_sql(f"SELECT * from {table}", self.conn)
@@ -165,12 +167,17 @@ class MenuScreen(Screen):
         self.manager.current = "LoginScreen"
 
 class TransactionsScreen(Screen):
+    def find_transaction(self, button):
+        self.parent.get_screen("ViewTransactionScreen").load_transaction(button.text, self.transactions)
+        self.manager.transition.direction = "left"
+        self.manager.transition.duration = 1
+        self.manager.current = "ViewTransactionScreen"
     def load_transactions(self, app):
-        self.sorted = None
         transactions = app.db.fetch("Transactions")
         if len(transactions) == 0:
             return
-        #transactions.transaction_date = transactions.transaction_date.apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d')) 
+        #transactions.transaction_date = transactions.transaction_date.apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
+        self.sorted = {"ascending":True}
         transactions.sort_values("transaction_date", inplace=True)
         transactions.transaction_id = transactions.index
         self.transactions = transactions
@@ -178,9 +185,10 @@ class TransactionsScreen(Screen):
         gl.clear_widgets()
         gl.size_hint_y = len(transactions)
         for row in transactions.values:
-            gl.add_widget(BubbleButton(text=f"{row[0]}"))
+            gl.add_widget(BubbleButton(text=f"{row[0]}", on_press=self.find_transaction))
             for item in row[1:]:
                 gl.add_widget(Label(text=str(item)))
+        return
     def sort_table(self, button):
         translate_columns = {
             "ID":"transaction_id",
@@ -191,28 +199,53 @@ class TransactionsScreen(Screen):
             "Tag":"transaction_tag",
             "Wallet":"wallet_name"
         }
-        print(self.sorted)
-        print(button.text)
-        if self.sorted == None or self.sorted != button.text:
+        if self.sorted["ascending"] == True:
             transactions = self.transactions.sort_values(translate_columns[button.text], ascending=False)
-        elif self.sorted == button.text:
-            transactions = self.transactions.sort_values(translate_columns[button.text])
-        self.sorted = button.text
+            self.sorted["ascending"] = False
+
+        elif self.sorted["ascending"] == False:
+            transactions = self.transactions.sort_values(translate_columns[button.text], ascending=True)
+            self.sorted["ascending"] = True
+        
         gl = self.children[0].children[1].children[0]
         gl.clear_widgets()
         gl.size_hint_y = len(transactions)
         for row in transactions.values:
-            gl.add_widget(BubbleButton(text=f"{row[0]}"))
+            gl.add_widget(BubbleButton(text=f"{row[0]}", on_press=self.find_transaction))
             for item in row[1:]:
                 gl.add_widget(Label(text=str(item)))
+        return
 
 
-
+class ViewTransactionScreen(Screen):
+    def load_transaction(self, transaction_num, dataframe): 
+        transaction = dataframe.loc[dataframe.transaction_id == int(transaction_num)]
+        gl = self.children[0].children[1]
+        lst = [str(i) for i in transaction.values[0]]
+        labels = ["Transaction ID", "Date", "Amount", "Location", "Type", "Tag", "Wallet"]
+        lst.reverse()
+        for gl_label,new_text in zip([child for child in gl.children if child.text not in labels],lst):
+            gl_label.text = new_text
+    
+    def delete_transaction(self, app, transaction_num):
+        app.db.delete("Transactions", int(transaction_num))
+        cb = BubbleButton(text="Transaction Deleted\n\n\n\n   Close")
+        pu = Popup(title="Success", content=cb, size_hint=(.5, .5))
+        cb.bind(on_press=pu.dismiss)
+        pu.open()
+        gl = self.children[0].children[1]
+        labels = ["Transaction ID", "Date", "Amount", "Location", "Type", "Tag", "Wallet"]
+        for gl_label in gl.children:
+            if gl_label not in labels:
+                gl_label.text = ""  
+        
     
 class AddTransactionScreen(Screen):
     def today_timestamp(self):
         return f"{datetime.datetime.now().date()}"
-    def update_db(self, app):
+    def update_db(self, app, schedule=False, new_date=False):
+        if schedule == True:
+            return
         lst = ["Wallet Name", "Tag", "Transaction Type", "Amount", "Date", "Location"]
         uinputs = {i:v.text for i,v in zip(lst, [v for v in self.children[0].children[1].children if v.text not in lst])}
         try:
@@ -224,7 +257,6 @@ class AddTransactionScreen(Screen):
             pu.open()
             return
         #make this a calender
-        print(uinputs["Date"])
         try:
             datetime.datetime.strptime(uinputs["Date"], '%Y-%m-%d')
         except:
@@ -247,6 +279,115 @@ class AddTransactionScreen(Screen):
         pu.open()
 
 
+class ScheduleScreen(Screen):
+    def clear_calender(self):
+        gl = self.children[0].children[1]
+        gl.clear_widgets()
+    def load_schedule(self, app):
+        today = datetime.datetime.now()
+        schedule = app.db.fetch("Scheduled")
+        self.calender= {
+            "1": "Jan",
+            "2": "Feb",
+            "3": "Mar",
+            "4": "Apr",
+            "5": "May",
+            "6": "Jun",
+            "7": "Jul",
+            "8": "Aug",
+            "9": "Sep",
+            "10": "Oct",
+            "11": "Nov",
+            "12": "Dec"
+        }
+        self.weekdays = day_name= ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
+        self.schedule = schedule
+        self.year = today.year
+        self.children[0].children[3].text = str(self.year)
+        self.children[0].children[2].children[1].text = self.calender[str(today.month)]
+        gl = self.children[0].children[1]
+        days_in_month = (datetime.date(today.year, today.month + 1, 1) - datetime.date(today.year, today.month, 1)).days
+        for i in range(1,8):
+            gl.add_widget(Label(text=f"{self.weekdays[datetime.date(today.year, today.month, i).weekday()]}"))
+
+        for i in range(1,days_in_month+1):
+            bb = BubbleButton(text=f"{i}", on_press=self.view_day)
+            bb.halign = "center"
+            bb.valign = "middle"
+            bb.background_color =  (.4, .5, 100, .3)
+            bb.text_size = (20,20)
+            gl.add_widget(bb)
+        self.selected_month = today.month
+    
+    def view_day(self, button):
+        self.manager.get_screen("DayScreen").load_day(self.schedule, self.year, self.selected_month, button.text)
+        self.manager.transition.direction = "left"
+        self.manager.transition.duration = 1
+        self.manager.current = "DayScreen"
+
+    def change_month(self, button):
+        today = datetime.datetime.now()
+        current_month = int([i for i,v in self.calender.items() if v == self.children[0].children[2].children[1].text][0])
+        gl = self.children[0].children[1]
+        year_label = self.children[0].children[3]
+        gl.clear_widgets()
+        if button.text == "<":
+            current_month -= 1
+            if current_month == 1:
+                self.year -= 1
+                year_label.text = str(self.year)
+                days_in_month = (datetime.date(self.year + 1, 1 , 1) - datetime.date(self.year, 12, 1)).days
+                self.children[0].children[2].children[1].text = self.calender["12"]
+            else:
+                days_in_month = (datetime.date(self.year, current_month +1 , 1) - datetime.date(self.year, current_month, 1)).days
+                self.children[0].children[2].children[1].text = self.calender[str(current_month)]
+            for i in range(1,8):
+                gl.add_widget(Label(text=f"{self.weekdays[datetime.date(self.year, current_month, i).weekday()]}"))
+
+            for i in range(1,days_in_month+1):
+                bb = BubbleButton(text=f"{i}", on_press=self.view_day)
+                bb.halign = "center"
+                bb.valign = "middle"
+                bb.text_size = bb.size
+                gl.add_widget(bb)
+        elif button.text == ">":
+            current_month += 1
+            if current_month == 12:
+                self.year += 1
+                year_label.text = str(self.year)
+                days_in_month = (datetime.date(self.year, 1, 1) - datetime.date(self.year - 1, 12, 1)).days
+                self.children[0].children[2].children[1].text = self.calender["12"]
+            elif current_month == 13:
+                current_month = 1
+                days_in_month = (datetime.date(self.year, current_month + 1, 1) - datetime.date(self.year, current_month, 1)).days
+                self.children[0].children[2].children[1].text = self.calender[str(current_month)]                
+            else:
+                days_in_month = (datetime.date(self.year, current_month + 1, 1) - datetime.date(self.year, current_month, 1)).days
+                self.children[0].children[2].children[1].text = self.calender[str(current_month)]
+            for i in range(1,8):
+                gl.add_widget(Label(text=f"{self.weekdays[datetime.date(self.year, current_month, i).weekday()]}"))            
+            for i in range(1, days_in_month+1):
+                bb = BubbleButton(text=f"{i}", on_press=self.view_day)
+                bb.halign = "center"
+                bb.valign = "middle"
+                bb.text_size = bb.size
+                gl.add_widget(bb)
+        self.selected_month = current_month
+
+
+class DayScreen(Screen):
+    def load_day(self, schedule, year, month, day):
+        print(schedule, year, month, day)
+        self.children[0].children[1].text = f"{year}-{month}-{day}"
+        if len(schedule) == 0:
+            return
+    def add_transaction(self, app):
+        self.manager.get_screen("AddTransactionScreen").update_db(app, schedule=True)
+        self.manager.transition.direction = "down"
+        self.manager.transition.duration = 1
+        self.manager.current = "AddTransactionScreen"
+        
+
 
 class MMApp(App):
     def build(self):
@@ -257,10 +398,9 @@ class MMApp(App):
         self.sm.add_widget(MenuScreen(name="MenuScreen"))
         self.sm.add_widget(TransactionsScreen(name="TransactionsScreen"))
         self.sm.add_widget(AddTransactionScreen(name="AddTransactionScreen"))
-        # self.sm.add_widget(ViewTransactionScreen(name="ViewTransactionScreen"))
-        #self.sm.add_widget(ScheduleScreen(name="ScheduleScreen"))
-        #self.sm.add_widget(DayScreen(name="DayScreen"))
-        
+        self.sm.add_widget(ViewTransactionScreen(name="ViewTransactionScreen"))
+        self.sm.add_widget(ScheduleScreen(name="ScheduleScreen"))
+        self.sm.add_widget(DayScreen(name="DayScreen"))        
         #check to see if User needs to be registered
         if self.db.register_user == True:
             self.sm.current = "RegistrationScreen"
